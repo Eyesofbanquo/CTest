e48801689b4974d0eff4e1eede7482ce48f8ed3a
mshaw24 <37548798+mshaw24@users.noreply.github.com>
2020-02-18 09:08:32 -0600

    Add Network object to project
    - Add network object to project
    
    - Add extension on URL so you can create URLs using string literals

---
+++ CodeTest/Layers/Network/Network.swift
@@ -0,0 +1,17 @@
+//
+//  Network.swift
+//  CodeTest
+//
+//  Created by Markim Shaw on 2/18/20.
+//  Copyright © 2020 Markim Shaw. All rights reserved.
+//
+
+import Foundation
+
+/// A class responsible for performing network calls against the Apple Music API
+final class Network {
+  
+}
+
+extension Network {
+}
